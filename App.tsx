d999d0ddb1ca5570c0fd2b3f6c85e6dcb151c84b
RennanD <rennan.douglas@objetivopi.com.br>
2022-01-11 17:44:28 -0300

    chore: configure styled-components and fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import AppLoading from 'expo-app-loading';
+
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+
 import { SingIn } from '@screens/SingIn';
 
 const styles = StyleSheet.create({
@@ -13,6 +18,15 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [fonstLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular,
+  });
+
+  if (!fonstLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <SingIn />
