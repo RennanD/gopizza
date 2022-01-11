d04eccf886dd82ee7a14d8740189fcca0d45099f
RennanD <rennan.douglas@objetivopi.com.br>
2022-01-11 16:56:22 -0300

    chore: init app

--- App.tsx
+++ App.tsx
@@ -2,15 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,3 +10,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  );
+}
